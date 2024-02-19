6a79d4fa4502f1f333dc7b385386eea95bbeb5d2
Setoichi Yumaden <118138305+TheDotBat@users.noreply.github.com>
2024-02-19 04:01:32 -0500

    Included new Install page to docs atlas

--- src/pages/docsSections.js
+++ src/pages/docsSections.js
@@ -1,6 +1,7 @@
 const docsSections = [
   { title: "Introduction", path: "/docs/introduction" },
   { title: "Getting Started", path: "/docs/getting-started" },
+  { title: "Install", path: "/docs/install" },
 ];
 
 export default docsSections;
